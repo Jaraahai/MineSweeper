10cad36e14647d80be2e155c8f81602e50ea7a5b
Jaraahai <pcatstwo@Gmail.com>
2023-11-15 18:00:07 +0800

    Hosted to firebase. Fixed few bugs.

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3u7jlLzFdqe6JEGx2wDYvIInTfjrgNo8",
+  authDomain: "minesweeper-e81a2.firebaseapp.com",
+  projectId: "minesweeper-e81a2",
+  storageBucket: "minesweeper-e81a2.appspot.com",
+  messagingSenderId: "1031507998793",
+  appId: "1:1031507998793:web:f9a1d9edeff9e1485f2798",
+  measurementId: "G-8VHTCMVR2R"
+};
+
+const app = initializeApp(firebaseConfig);
